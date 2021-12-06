72a23f49537d4e60ca3f7704e7b3764c01c107be
samar03-gif <94772832+samar03-gif@users.noreply.github.com>
2021-12-06 18:36:19 +0530

    Add files via upload

---
+++ resume.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+class resume{
+	public static void intro(){
+		System.out.println("--------------------------------------INFORMATION---------------------------------");
+		System.out.println("Name-Samar Soni");
+		System.out.println("Contact - 6268523291");
+		System.out.println("E-mail = samarsoninewapp@gmail.com");
+		System.out.println("__________________________________________________________________________________");
+		System.out.println("__________________________________________________________________________________");
+
+	}
+	public static void career_object(){
+		System.out.println("-----------------------------------CAREER OBJECT ---------------------------------");
+		System.out.println("To seek a dynamic and challenging career an organization strives for excellience \nwith my knowledge and team effort while making posible contribution to \npromote the individual opportunity and professional growth");
+		System.out.println("__________________________________________________________________________________");
+		System.out.println("__________________________________________________________________________________");
+	}
+	public static void acadamic_qualification(){
+		System.out.println("---------------------------------ACADAMIC QUALIFICATION---------------------------");
+		System.out.println("Sr.no          Class        Board/University       Year           DIVISION/Percentage");
+		System.out.println("1              High School    M.P. Board           2017            2st Division");
+		System.out.println("2              Intermediate   M.P. Board           2020            2st Division");
+		System.out.println("3              B.E.(CSE)      UIT BU               2020-2024       Running");
+		System.out.println("__________________________________________________________________________________");
+		System.out.println("__________________________________________________________________________________");
+	}
+	public static void skills(){
+		System.out.println("------------------------------------SKILLS----------------------------------------");
+		System.out.println("1.Python Programming Language");
+		System.out.println("2.C Programming Language");
+		System.out.println("3.C++ Programming Language");
+		System.out.println("4.Java Programming Language");
+		System.out.println("5.Data Visualization with Python");
+		System.out.println("6.HTML");
+		System.out.println("7.CSS");
+		System.out.println("8.Java Script");
+		System.out.println("9.Basic Computer knowledge");
+		System.out.println("_________________________________________________________________________________");
+		System.out.println("_________________________________________________________________________________");
+	}
+	public static void factor(){
+		System.out.println("-------------------------------Persnal Information-------------------------------");
+		System.out.println("Father Name - Shrikant Soni");
+		System.out.println("Date of Birth - 16/03/2000");
+		System.out.println("Marital Status - Unmarried");
+		System.out.println("Genral -  Male");
+		System.out.println("Religion - Hindu");
+		System.out.println("Nationality - India");
+		System.out.println("________________________________________________________________________________");
+		System.out.println("________________________________________________________________________________");
+	}
+	public static void project(){
+		System.out.println("---------------------------------PROJECT----------------------------------------");
+	    System.out.println("1.basic calculater gui : Built a basic calculater using java");
+		System.out.println("2.Google Translate : Build a basic google translate using python");
+		System.out.println("3.Bus reservation system :Build a basic bus reservation system using cpp");
+		System.out.println("4.Database managment system : Build a basic dbms system using cpp");
+		System.out.println("________________________________________________________________________________");
+		System.out.println("________________________________________________________________________________");
+	}
+	public static void declaration(){
+		System.out.println("---------------------------------DECLARATION------------------------------------");
+		System.out.println("I do hereby declare that the statments made in this document are true to the best\nof my knowledge and belief");
+		System.out.println("________________________________________________________________________________");
+		System.out.println("________________________________________________________________________________");
+	}
+	public static void main(String args[]){
+
+
+		System.out.println("-------------------------------------RESUME-----------------------------------\n");
+		intro();
+		factor();
+		career_object();
+		acadamic_qualification();
+		skills();
+		project();
+		declaration();
+	}
+}
